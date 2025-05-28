42eb9ade41a92e43ca6fce125eaac899687fd663
Singanapallicharan <charansinganapalli59@gmail.com>
2025-05-28 16:39:07 +0530

    Updated settings and requirements

--- emailotp/settings.py
+++ emailotp/settings.py
@@ -37,17 +37,23 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
     'otp',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://emailotp-xraz.onrender.com",
 ]
 
 ROOT_URLCONF = 'emailotp.urls'
